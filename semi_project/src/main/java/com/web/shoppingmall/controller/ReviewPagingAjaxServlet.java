d50fbbeebc45c35fecb7c120ebcd2576252e7de3
GD <GD@DESKTOP-TOJ0JQP>
2024-05-22 17:25:34 +0900

    GJ - 리뷰 모달창 완성, 리뷰페이징처리 진행중

---
+++ semi_project/src/main/java/com/web/shoppingmall/controller/ReviewPagingAjaxServlet.java
@@ -0,0 +1,40 @@
+package com.web.shoppingmall.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReviewPagingAjaxServlet
+ */
+@WebServlet("/shoppingmall/reviewpagingajax.do")
+public class ReviewPagingAjaxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReviewPagingAjaxServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
